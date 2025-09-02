972e246f69f5dce5645364e268db4f5d015a7543
Anastasiya Trayanava <anastasiya.trayanava@ensek.co.uk>
2025-09-02 13:16:44 +0200

    update app settings

--- src/ApplicationCore/Services/OrderService.cs
+++ src/ApplicationCore/Services/OrderService.cs
@@ -69,6 +69,7 @@ public class OrderService : IOrderService
 
     private async Task SendOrderDetailsToProcessor(Order order)
     {
+        return;
         var finalPrice = Enumerable.Sum(order.OrderItems.Select(x => x.Units * x.UnitPrice));
         var orderDetails = new OrderDetails()
         {
